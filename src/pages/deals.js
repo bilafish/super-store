72b2a6e5a251ac37ee306c47b2eba4a33d3124ab
Jason <bilafish@gmail.com>
2021-02-25 00:58:17 +0800

    feat: integrate deals page with api

--- src/pages/deals.js
+++ src/pages/deals.js
@@ -1,10 +1,63 @@
 import Header from "components/Header";
+import { useEffect, useState } from "react";
+import { Box, Flex } from "@chakra-ui/react";
+import fetchItems from "services/items";
+import ProductCard from "components/Home/ProductCard";
+import Pagination from "components/Pagination";
+
 const Deals = () => {
+  // COMPONENT STATES
+  const [products, setProducts] = useState([]);
+  const [hasNext, setHasNext] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [lastPage, setLastPage] = useState(1);
+  const paginationSize = 10;
+  // USEEFFECTS
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const result = await fetchItems({
+          pageSize: paginationSize,
+          startIndex: (currentPage - 1) * paginationSize,
+          isOnSale: true,
+        });
+        setProducts(result.items);
+        setHasNext(result.hasMore);
+        setLastPage(Math.ceil(result.total / paginationSize));
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchProducts();
+  }, [currentPage]);
   return (
-    <div>
+    <>
       <Header />
-      <h1>Deals</h1>
-    </div>
+      <Box
+        width="100%"
+        height="calc(100vh - 68px)"
+        px="5rem"
+        pb="5rem"
+        position="fixed"
+        top="68px"
+        bottom="0"
+        overflowY="auto"
+        textAlign="center"
+      >
+        <Flex flexWrap="wrap" justifyContent="space-between">
+          {products.map((item) => (
+            <ProductCard key={item._id} data={item} />
+          ))}
+        </Flex>
+        <Pagination
+          currentPage={currentPage}
+          pageSize={paginationSize}
+          hasNext={hasNext}
+          lastPage={lastPage}
+          setCurrentPage={setCurrentPage}
+        />
+      </Box>
+    </>
   );
 };
 export default Deals;
--- src/services/items.js
+++ src/services/items.js
@@ -1,6 +1,7 @@
-const fetchItems = ({ pageSize, startIndex }) => {
+const fetchItems = ({ pageSize, startIndex, isOnSale }) => {
+  const isOnSaleQuery = isOnSale === true ? "&isOnSale=true" : "";
   return fetch(
-    `https://gp-super-store-api.herokuapp.com/item/list?size=${pageSize}&from=${startIndex}`
+    `https://gp-super-store-api.herokuapp.com/item/list?size=${pageSize}&from=${startIndex}${isOnSaleQuery}`
   )
     .then((res) => res.json())
     .then((json) => json)
